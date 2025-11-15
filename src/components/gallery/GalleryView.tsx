596a07c89132c4f658fdc2346a516d1cf7c9e940
Jesus Aguilar <aguilarandradejesus@gmail.com>
2025-11-15 12:24:47 -0500

    Only the first two images in gallery get eagerly loaded

--- src/components/gallery/GalleryView.tsx
+++ src/components/gallery/GalleryView.tsx
@@ -9,13 +9,14 @@ export default function GalleryView() {
       {gallery.images.map((image, index) => (
         <Media
           enlarge
-          priority={index < 10}
+          priority={index < 2}
           sizes="(max-width: 560px) 100vw, 50vw"
           key={index}
           radius="m"
           aspectRatio={image.orientation === "horizontal" ? "16 / 9" : "3 / 4"}
           src={image.src}
           alt={image.alt}
+          style={{ imageOrientation: "none" }}
         />
       ))}
     </MasonryGrid>
